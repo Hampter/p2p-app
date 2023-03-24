780a8e648fb994c1974b6f04a2e5ab2f86a9d096
Noah Springer <23213489+Hampter@users.noreply.github.com>
2023-03-24 16:02:39 -0400

    testing

--- src/app/recieve/recieve.component.ts
+++ src/app/recieve/recieve.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { P2pService } from '../_services/p2p.service';
+
 
 @Component({
   selector: 'app-recieve',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./recieve.component.scss']
 })
 export class RecieveComponent {
+  port = '';
 
+  constructor(private p2p: P2pService) {}
+
+  portUpdate(value: string) {
+    this.port = value;
+  }
+
+  recieveFile() {
+    if (this.port) {
+      const download$ = this.p2p.recieveFile(this.port);
+      download$.subscribe((e) => {
+        console.log(e);
+      })
+    }
+  }
 }
